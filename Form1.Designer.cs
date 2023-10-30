13f656cce240e32d4eaf09f84fa5f4cdf9b98efb
null <null>
2023-10-30 12:42:32 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Ejercicio16_PilasYColas_G2_2024_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtbPalindromos = new System.Windows.Forms.TextBox();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.lbFrase = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(127, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(170, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "¡PALINDROMOS!";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(149, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(121, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Introduzca su frase:";
+            // 
+            // txtbPalindromos
+            // 
+            this.txtbPalindromos.Location = new System.Drawing.Point(80, 105);
+            this.txtbPalindromos.Name = "txtbPalindromos";
+            this.txtbPalindromos.Size = new System.Drawing.Size(263, 20);
+            this.txtbPalindromos.TabIndex = 2;
+            this.txtbPalindromos.TextChanged += new System.EventHandler(this.txtbPalindromos_TextChanged);
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAceptar.Location = new System.Drawing.Point(152, 160);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(118, 43);
+            this.btnAceptar.TabIndex = 3;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            this.btnAceptar.Click += new System.EventHandler(this.btnAceptar_Click);
+            // 
+            // lbFrase
+            // 
+            this.lbFrase.AutoSize = true;
+            this.lbFrase.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Italic | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbFrase.Location = new System.Drawing.Point(141, 230);
+            this.lbFrase.Name = "lbFrase";
+            this.lbFrase.Size = new System.Drawing.Size(142, 18);
+            this.lbFrase.TabIndex = 4;
+            this.lbFrase.Text = "¿Es un Palíndromo?";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(407, 307);
+            this.Controls.Add(this.lbFrase);
+            this.Controls.Add(this.btnAceptar);
+            this.Controls.Add(this.txtbPalindromos);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtbPalindromos;
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.Label lbFrase;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Ejercicio16_PilasYColas_G2_2024_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        static void Cola(object txtbPalindromos)
+        {
+            Queue<String> FraseInicial = new Queue<String>();
+
+            txtbPalindromos = char.Parse(txtbPalindromos.ToString());
+
+            int total = FraseInicial.Count; 
+            for (int i = 0; i < total; i++)
+            {
+
+            }
+        }
+        static void Pila()
+        {
+            Stack<String> FraseReversa = new Stack<String>();
+
+        }
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+            if ()
+            {
+                lbFrase.Text = "Es un Palindromo!";
+            }
+            else
+            {
+                lbFrase.Text = "No es un Palindromo!";
+            }
+        }
+
+        private void txtbPalindromos_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        
+    }
+}
